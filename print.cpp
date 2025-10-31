a990a5e522fa5111dbff94195f66954cc4fd93d9
Zach <100164907@ccisd.net>
2025-10-31 12:43:19 -0500

    Create print.cpp

---
+++ print.cpp
@@ -0,0 +1,11 @@
+// -----------------------------
+// --- THIS FILE IS OWNED BY ---
+// ---    LUNAR LANGUAGE     ---
+// -----------------------------
+
+#include "Print.h" // Include the header file we just defined
+
+void print(const char* text) {
+    // We can use the standard C library function printf for the actual printing.
+    printf("%s\n", text);
+}
