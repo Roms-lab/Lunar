8cd5a20ac87a1b1dda4e8aa0706ff1faa38f9d6a
Zach <100164907@ccisd.net>
2025-10-31 12:37:34 -0500

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+// -----------------------------
+// --- THIS FILE IS OWNED BY ---
+// ---    LUNAR LANGUAGE     ---
+// -----------------------------
+
+#include "Print.h"
+
+int main() {
+
+	return 0;
+}
